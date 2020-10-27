95956b64887d308537282175c5533f17211e4bf9
Michael Chiang <mychiang13@gmail.com>
2020-10-27 14:48:54 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Impostergram-DB-Project
+ CS 179g project Fall 2020
---
+++ loginscreen.py
@@ -0,0 +1,23 @@
+#imports ...
+
+print("Welcome to Impostergram\nPlease log in.")
+loop=True
+while loop:
+    username=input("Username:")
+    password=input("Password:")
+    valid_login=False
+    #query to see if valid combination of password and username
+    #valid_login bool will change depending on query
+    if valid_login:
+        print("Successful Login Proceeding. Proceeding to menu.")
+        #go to user menu class
+        #pass along user token i.e. Username/Password
+        #as they should only be allowed to do tasks related to their account
+    else:
+        print("Invalid information entered. Would you like to try again?")
+        con=input("(Y/N):")
+        if con =="n" or con=="N" or con=="no" or con=="No":
+            loop=False
+print("GoodBye")
+
+
