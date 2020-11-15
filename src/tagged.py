a157051f6122c67cbdcb9430e76bca20b7ca178f
TinhLa72 <52975703+TinhLa72@users.noreply.github.com>
2020-11-14 17:30:47 -0800

    Update tagged.py

--- src/tagged.py
+++ src/tagged.py
@@ -1,40 +1,46 @@
 import psycopg2
+from postdb import post_db
 
 class tagged:
     def __init__(self, photo_id):
         __photo_id = photo_id
-        conn=None
-        try:
-            #connect to the postgresql db
-            conn = psycopg2.connect(
-                host="localhost",
-                database="impostergram_db",
-                user="postgres",
-                password="postgres"
-            )            
-        except (Exception, psycopg2.DatabaseError) as error:
-            print(error)
-        finally:
-            pass
+        post = post_db()
+        cur = post.conn.cursor()
    
     def tag(self):
         validity=False
-        try:
-            username = input('Please enter a username: ')
-            cur=self.conn.cursor()
-            cur.execute("SELECT username FROM Users WHERE username = %s", (username))
-            if cur.rowcount > 0:
-                validity = True
-            if validity: 
-                cur.execute("INSERT INTO Tagged(username,photo_id) VALUES(%s, %s)", (username, self.__photo_id))
-
-        except (Exception,psycopg2.DatabaseError) as error:
-            print(error)
-        finally:
-             if cur is not None:
-                cur.close()
-                print("Closing cursor")
-                if self.conn is not None:
-                    self.conn.close()
+        loop = True
+        while(loop):
+            try:
+                username = input('Please enter a username: ')
+                self.cur.execute("SELECT username FROM Users WHERE username = %s", (username))
+                if self.cur.rowcount > 0:
+                    validity = True
+                else: 
+                    choice = input('User not found.\nWould you like to try again? (Y/N): ')
+                    if (choice == 'N' or choice == 'n'):
+                        loop = False
+                        if self.cur is not None:
+                            self.cur.close()
+                            print("Closing cursor")
+                        if self.post.conn is not None:
+                            self.post.conn.close()                   
+                if validity: 
+                    self.cur.execute("INSERT INTO Tagged (username,photo_id) VALUES (%s, %s)", (username, self.__photo_id))
+                    if self.cur is not None:
+                        self.cur.close()
+                        print("Closing cursor")
+                    if self.post.conn is not None:
+                        self.post.conn.close()
+                        # print("Closing database connection")
+                    return
+            except (Exception,psycopg2.DatabaseError) as error:
+                print(error)
+                if self.cur is not None:
+                    self.cur.close()
+                    print("Closing cursor")
+                if self.post.conn is not None:
+                    self.post.conn.close()
+                return
                     # print("Closing database connection")
-        return validity
+        return
