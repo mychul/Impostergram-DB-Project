19eef811d8776af05c4c81c6de8976b1b89d07a1
Michael Chiang <mychiang13@gmail.com>
2020-11-14 17:51:59 -0800

    updated loginscreen and postdb

--- src/loginscreen.py
+++ src/loginscreen.py
@@ -1,21 +1,43 @@
 #imports ...
-from postdb import postdb
+import psycopg2
+from postdb import post_db
+from usermenu import usermenu
 
+def validation_login(self,u_name,u_pass):
+        post= post_db() # create a postdb object of the class post_db
+        validity=False
+        try:
+            cur=post.conn.cursor() # create a cursor
+            cur.execute("SELECT username, pass FROM Users WHERE username = %s, pass =%s",(u_name,u_pass)) #query for the username and password for validation
+            if cur.rowcount == 1: # if that combination was found than rowcount would be 1
+                validity =True
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+            return False
+        finally:
+            if cur is not None:
+                cur.close()
+                print("Closing cursor")
+            if post.conn is not None:
+                post.conn.close()
+                print("Closing database connection")
+            del post #cleanup of the post object
+        return validity
 
+#starting point
 print("Welcome to Impostergram\nPlease log in.")
 loop=True
 while loop:
+    choice = input("Would you like to log in(1) or exit(2): ") 
+    if choice == 2:
+        break
     username=input("Username:")
     password=input("Password:")
     valid_login=False
-    #query to see if valid combination of password and username
-    #valid_login bool will change depending on query
+    valid_login=validation_login(username,password) #validate given login information
     if valid_login:
-        print("Successful Login Proceeding. Proceeding to menu.")
-        #go to user menu class
-        #pass along user token i.e. Username/Password
-        #as they should only be allowed to do tasks related to their account
-        #valid_login = post.login(username,password)
+        print("Successful Login. Proceeding to main menu.")
+        menu=usermenu()
     else:
         print("Invalid information entered. Would you like to try again?")
         con=input("(Y/N):")
