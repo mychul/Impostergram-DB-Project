c6cd67bc0d7c2c2a16cfb8466b608d50eaf9560c
TinhLa72 <52975703+TinhLa72@users.noreply.github.com>
2020-11-14 16:42:14 -0800

    Update tagged.py

--- src/tagged.py
+++ src/tagged.py
@@ -1,7 +1,8 @@
 import psycopg2
 
 class tagged:
-    def __init__(self):
+    def __init__(self, photo_id):
+        __photo_id = photo_id
         conn=None
         try:
             #connect to the postgresql db
@@ -10,24 +11,30 @@ class tagged:
                 database="impostergram_db",
                 user="postgres",
                 password="postgres"
-            )
-            
+            )            
         except (Exception, psycopg2.DatabaseError) as error:
             print(error)
         finally:
-            if self.conn is not None:
-                self.conn.close()
-                print("Closing database connection")
-                
-    def login(self,u_name,u_pass):
+            pass
+   
+    def tag(self):
         validity=False
         try:
+            username = input('Please enter a username: ')
             cur=self.conn.cursor()
-            cur.execute("SELECT username, pass FROM Users WHERE username = %s, pass =%s",(u_name,u_pass))
+            cur.execute("SELECT username FROM Users WHERE username = %s", (username))
+            if cur.rowcount > 0:
+                validity = True
+            if validity: 
+                cur.execute("INSERT INTO Tagged(username,photo_id) VALUES(%s, %s)", (username, self.__photo_id))
+
         except (Exception,psycopg2.DatabaseError) as error:
             print(error)
         finally:
              if cur is not None:
                 cur.close()
                 print("Closing cursor")
+                if self.conn is not None:
+                    self.conn.close()
+                    # print("Closing database connection")
         return validity
