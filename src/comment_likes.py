3d07d5ae7590ae83bec303cecb6ebf5e25a4da12
TinhLa72 <52975703+TinhLa72@users.noreply.github.com>
2020-11-16 20:58:34 -0800

    Create comment_likes.py

---
+++ src/comment_likes.py
@@ -0,0 +1,66 @@
+import psycopg2
+from postdb import post_db
+
+class comment_likes:
+    def __init__(self, username, comment_id):
+        __comment_id = comment_id
+        __username = username
+        post = post_db()
+        cur = post.conn.cursor()
+   
+    # Let user menu worry about the 'would you like to enter another comment'
+    def likes(self):
+        try:
+            self.cur.execute("SELECT username FROM Likes WHERE username = %s AND comment_id = %s", (self.__username, self.__comment_id))
+            if self.cur.rowcount > 0:
+                print("Error: You've already liked this comment.  (-_-) ")
+            elif self.cur.rowcount <= 0:
+                self.cur.execute("INSERT INTO Likes (username, comment_id) VALUES (%s, %s)", (self.__username, self.__comment_id))            
+                print("Successfully liked the comment.")
+                self.post.conn.commit()
+        except (Exception,psycopg2.DatabaseError) as error:
+            print(error)
+            if self.cur is not None:
+                self.cur.close()
+                print("Closing cursor")
+            if self.post.conn is not None:
+                self.post.conn.close()
+            del self.post
+            return
+        finally: 
+            if self.cur is not None:
+                self.cur.close()
+                print("Closing cursor")
+            if self.post.conn is not None:
+                self.post.conn.close()
+            del self.post
+            # print("Closing database connection")
+        return
+
+    def unlikes(self):
+        try:
+            self.cur.execute("SELECT username FROM Likes WHERE username = %s AND comment_id = %s", (self.__username, self.__comment_id))
+            if self.cur.rowcount <= 0:
+                print("Error: You have not liked this comment.  (-_-) ")
+            elif self.cur.rowcount > 0:
+                self.cur.execute("DELETE FROM Likes WHERE username = %s AND comment_id = %s", (self.__username, self.__comment_id))            
+                print("Successfully unliked the comment.")
+                self.post.conn.commit()
+        except (Exception,psycopg2.DatabaseError) as error:
+            print(error)
+            if self.cur is not None:
+                self.cur.close()
+                print("Closing cursor")
+            if self.post.conn is not None:
+                self.post.conn.close()
+            del self.post
+            return
+        finally: 
+            if self.cur is not None:
+                self.cur.close()
+                print("Closing cursor")
+            if self.post.conn is not None:
+                self.post.conn.close()
+            del self.post
+            # print("Closing database connection")
+        return
