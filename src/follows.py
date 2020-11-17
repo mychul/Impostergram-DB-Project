1be27328381ad6a74f6fbb985e8f73585898c07a
mingi-lee <43664964+mingi-lee@users.noreply.github.com>
2020-11-17 15:06:42 -0800

    Updata follows.py
    fix addFollow function and make a new function(delFollow)

--- src/follows.py
+++ src/follows.py
@@ -10,13 +10,18 @@ class follows:
             
     def addFollow(self):
         validity = False
+        validity_user = False
         try:
             while(not validity):
-                u_name2 = input("Please enter a username to follow : ")
+                u_name2 = input("Please enter a username to Follow : ")
 #                cur = self.conn.cursor()
+                if u_name2 == self.__u_name1:
+                        print("You put yourself! Try it again")
+                        continue
+
                 self.cur.execute("SELECT username FROM Users WHERE username = %s",(u_name2))
                 if self.cur.rowcount > 0:
-                    validity = True
+                    validity_user = True
                 else:
                     print("That username does not exist!")
                     repeat = input("Do you want to enter a username again?[Y/N] : ")
@@ -26,14 +31,49 @@ class follows:
                             break
                         print("You put wrong answer")
                         repeat = input("Do you want to enter a username again?[Y/N] : ")                    
+                if validity_user:
+                    self.cur.execute("SELECT * FROM Follows WHERE username1 = %s, username2 = %s", (self.__u_name1, u_name2))
+                    if self.cur.rowcount > 0:
+                        print("You already followed %s", u_name2)
+                    else:
+                        self.cur.execute("INSERT INTO Follows(username1,username2) VALUES(%s, %s)", (self.__u_name1, u_name2))
+                    valitidy = True
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
 
-            if validity:
-                self.cur.execute("INSERT INTO Follows(username1,username2) VALUES(%s, %s)", (self.__u_name1, u_name2))
-                validity = False
+        finally:
+            if self.cur is not None:
+                self.cur.close()
+                print("Closing cursor")
                 if self.post.conn is not None:
                     self.post.conn.close()
                     print("Closing database connection")
-
+    def delFollow(self):
+        validity = False
+        validity_user = False
+        try:
+            while(not validity):
+                u_name2 = input("Please enter a username to Unfollow : ")
+                if u_name2 == self.__u_name1:
+                        print("You put yourself! Try it again")
+                        continue
+#                cur = self.conn.cursor()
+                self.cur.execute("SELECT * FROM Follows WHERE username1 = %s, username2 = %s",(self.__u_name1, u_name2))
+                if self.cur.rowcount > 0:
+                    validity = True
+                else:
+                    print("That follow does not exist!")
+                    repeat = input("Do you want to enter a username again?[Y/N] : ")
+                    while(not repeat == "Y" and not repeat == "y"):
+                        if(repeat == "N" or repeat == "n"):
+                            break
+                            break
+                        print("You put wrong answer")
+                        repeat = input("Do you want to enter a username again?[Y/N] : ")                    
+                if validity:
+                    self.cur.execute("DELETE * FROM Follows WHERE username1 = %s, username2 = %s", (self.__u_name1, u_name2))
+                    validity = False
+                    
         except (Exception, psycopg2.DatabaseError) as error:
             print(error)
 
