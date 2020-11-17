4ef184a17630ba9f5381cab1d3e638a0f3f0c519
TinhLa72 <52975703+TinhLa72@users.noreply.github.com>
2020-11-16 21:37:00 -0800

    Update files
    commentLikes: created class
    view_comments: wrote code to implement commentLikes's Like/Unlike

--- src/view_comments.py
+++ src/view_comments.py
@@ -1,3 +1,4 @@
+from commentLikes import comment_likes
 import psycopg2
 from postdb import post_db
 
@@ -22,17 +23,32 @@ class view_comments:
                 comment_id.append(row[0])
             choice = input("Would you like to \"Like\/Unlike\" a comment? (Y/N): ")
             if(choice == "Y" or choice == "y"):
-                valid = False
-                while(not valid):
-                    comment_choice = input("Please enter the comment's id: ") 
+                loop = True
+                while(loop):
+                    comment_choice = input("Please enter the comment's id (-1 to Cancel): ")
+                    if(comment_choice == -1):
+                        loop = False
+                        continue
+                    valid = False 
                     for temp in comment_id:
                         if(comment_choice == comment_id):
                             valid = True
                             break
                     if(valid):
-                        # create a Comment_Like object passing in comment_choice and username
-                        # delete the object
-                        pass
+                        choice2 = input("Enter 1 to Like, 0 to UnLike (-1 to Cancel): ")
+                        if(choice2 == -1):
+                            loop = False
+                            continue
+                        if(choice2 == 1):
+                            # create a Comment_Like object passing in comment_choice and username into the appropoate function
+                            temp = comment_likes(self.__username, comment_choice)
+                            temp.likes()
+                            del temp # delete the object
+                        elif(choice2 == 0):
+                            # create a Comment_Like object passing in comment_choice and username into the appropoate function
+                            temp = comment_likes(self.__username, comment_choice)
+                            temp.unlikes()
+                            del temp # delete the object
                     else:
                         print("Comment_id that you entered does not exist.")
                         valid = False
