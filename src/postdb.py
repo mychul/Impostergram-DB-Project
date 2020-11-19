07cc39f4a8d604a5d67911b4081b841d9b6605fb
Michael Chiang <mychiang13@gmail.com>
2020-11-19 13:51:20 -0800

    Update postdb.py

--- src/postdb.py
+++ src/postdb.py
@@ -5,12 +5,14 @@ class post_db:
         conn=None
         try:
             #connect to the postgresql db
+            print ("Attempting to create connection.")
             conn = psycopg2.connect(
                 host="localhost",
                 database="impostergram_db",
                 user="team2",
                 password="179g"
             )
+            print ("Success")
             
         except (Exception, psycopg2.DatabaseError) as error:
             print(error)
