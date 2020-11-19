1edc5561540075b0af16f9edf1a593f580f12b43
Michael Chiang <mychiang13@gmail.com>
2020-11-19 14:54:57 -0800

    Create testing.py

---
+++ src/testing.py
@@ -0,0 +1,22 @@
+import psycopg2
+
+conn=None
+cur =None
+try:
+    #connect to the postgresql db
+    print ("Attempting to create connection.")
+    conn = psycopg2.connect(
+        host="localhost",
+        database="impostergram_db",
+        user="team2",
+        password="179g"
+    )
+    print ("Successfully made connection")
+    cur.execute("SELECT * FROM Users")
+    rows =cur.fetchall()
+    for x in rows:
+        print (x)    
+except (Exception, psycopg2.DatabaseError) as error:
+    print(error)
+
+
