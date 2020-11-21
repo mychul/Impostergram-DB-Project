57dadbff82a777ca0fcb742bb893e2059871eada
TinhLa72 <52975703+TinhLa72@users.noreply.github.com>
2020-11-21 15:11:27 -0800

    Update testMongodb.py

--- src/testMongodb.py
+++ src/testMongodb.py
@@ -2,14 +2,14 @@ import pymongo
 from pymongo import MongoClient
 
 cluster = MongoClient("mongodb+srv://team2:179g@cluster0.fm94y.mongodb.net/Impostergram?retryWrites=true&w=majority")
-db = cluster["impostergram"]
+db = cluster["Impostergram"]
 collection = db["Photos"]
 
 post= {"_id":"p1"}
 
-collection.insert_one(post)
+#collection.insert_one(post)
 
-#result = collection.find({"_id":"p1"})
+result = collection.find({"_id":"p1"})
 
-#for x in result:
-    #print(x)
+for x in result:
+    print(x)
