ba5266c362dedff7bce5ef8e2723382a17791322
TinhLa72 <52975703+TinhLa72@users.noreply.github.com>
2020-12-01 14:18:26 -0800

    Update userSearch.py

--- src/userSearch.py
+++ src/userSearch.py
@@ -36,13 +36,10 @@ class user_search:
         self.conn_closed = True
         
     def userSearch(self):
-        validity_user = False
-        validity_photo = False
-        validity_description = False
         try:
             loop = True
             while(loop):
-                print("1. Search by username\n2. Search by Photo ID\n3. Search by Description")
+                print("1. Search by username\n2. Search by Photo ID\n3. Search by Description\n4. Search by greater than or equal to number of followers\n5. Search by greater than or equal to number of likes\n6. Search by the number of photos published")
                 select = input("Which option do you want to search? (-1. To Exit): ")
 
                 # For clearing the screen after user choice
@@ -59,7 +56,6 @@ class user_search:
                         continue
                     self.cur.execute("SELECT username FROM Users WHERE username = %s", [u_name2.lower()])
                     if self.cur.rowcount > 0:
-                        validity_user = True
                         print(u_name2 + " is an existing user.")
                         repeat = input("Do you want to search for another user? [Y/N]: ")
                         while(not repeat == "Y" and not repeat == "y"):
@@ -75,7 +71,6 @@ class user_search:
                     photoId = input("Please enter a Photo ID to search: ")
                     self.cur.execute("SELECT publisher FROM Photos WHERE photo_id = %s", [photoId])
                     if self.cur.rowcount > 0:
-                        validity_photo = True
                         tempString = self.cur.fetchone()
                         print("Publisher of Photo ID " + photoId + " is " + tempString[0])
                         repeat = input("Do you want to search for another user? [Y/N]: ")
@@ -93,12 +88,9 @@ class user_search:
                     sql = "%" + description + "%"
                     self.cur.execute("SELECT photo_id, publisher FROM Photos WHERE description LIKE %s", [sql])
                     if self.cur.rowcount > 0:
-                        validity_description = True
                         result_users = self.cur.fetchall()
-                        result = []
                         for row in result_users:
                             print("username: " + row[1] + " found in photo with id: " + row[0])
-                            result.append(row[0])
 #                        choice = input("Please check the user from this list")     #start from here
                         repeat = input("Do you want to search for another user? [Y/N]: ")
                         while(not repeat == "Y" and not repeat == "y"):
@@ -109,6 +101,76 @@ class user_search:
                             repeat = input("Do you want to search for another user? [Y/N]: ")
                     else:
                         print("That description does not exist in Photos.")
+                        continue
+                elif select == "4":
+                    
+                    fence = input("Enter the number of followers to search by: ")
+                    if fence != "0": 
+                        self.cur.execute("SELECT username FROM Users WHERE numFollows >= %s", ([fence]))
+                        if self.cur.rowcount > 0:
+                            result_users = self.cur.fetchall()
+                            for row in result_users:
+                                print("username: " + row[0])
+    #                        choice = input("Please check the user from this list")     #start from here
+                            repeat = input("Do you want to search for another user? [Y/N]: ")
+                            while(not repeat == "Y" and not repeat == "y"):
+                                if(repeat == "N" or repeat == "n"):
+                                    loop = False
+                                    break
+                            # print("You put wrong answer")
+                                repeat = input("Do you want to search for another user? [Y/N]: ")
+                        else:
+                            print("No users found.")
+                            continue
+                    else:
+                        print("Please do not enter zero.")
+                        continue
+                elif select == "5":
+                    
+                    fence = input("Enter the number of photo likes to search by: ")
+                    if fence != "0": 
+                        self.cur.execute("SELECT publisher FROM Photos WHERE numLikes >= %s GROUP BY publisher", ([fence]))
+                        if self.cur.rowcount > 0:
+                            result_users = self.cur.fetchall()
+                            for row in result_users:
+                                print("username: " + row[0])
+    #                        choice = input("Please check the user from this list")     #start from here
+                            repeat = input("Do you want to search for another user? [Y/N]: ")
+                            while(not repeat == "Y" and not repeat == "y"):
+                                if(repeat == "N" or repeat == "n"):
+                                    loop = False
+                                    break
+                            # print("You put wrong answer")
+                                repeat = input("Do you want to search for another user? [Y/N]: ")
+                        else:
+                            print("No users found.")
+                            continue
+                    else:
+                        print("Please do not enter zero.")
+                        continue
+                elif select == "6":
+                    
+                    fence = input("Enter the number of photos published: ")
+                    if fence != "0": 
+                        self.cur.execute("SELECT COUNT(photo_id), publisher FROM Photos GROUP BY publisher")
+                        if self.cur.rowcount > 0:
+                            result_users = self.cur.fetchall()
+                            for row in result_users:
+                                if int(row[0]) >= int(fence):
+                                    print("username: " + row[1])
+    #                       choice = input("Please check the user from this list")     #start from here
+                            repeat = input("Do you want to search for another user? [Y/N]: ")
+                            while(not repeat == "Y" and not repeat == "y"):
+                                if(repeat == "N" or repeat == "n"):
+                                    loop = False
+                                    break
+                            # print("You put wrong answer")
+                                repeat = input("Do you want to search for another user? [Y/N]: ")
+                        else:
+                            print("No users found.")
+                            continue
+                    else:
+                        print("Please do not enter zero.")
                         continue
         except (Exception,psycopg2.DatabaseError) as error:
             print(error)
